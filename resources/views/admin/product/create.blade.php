d32ac5b6880e2d4b51b3e48a78b9ad06810ad953
yemliha81 <yemlihaddln@hotmail.com>
2026-04-21 09:48:20 +0300

    product save fix

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -83,7 +83,7 @@
                                         </div>
                                         <div class="mb-3">
                                             <label for="category_id" class="form-label">Kategori ({{ $language->lang_code }})</label>
-                                            <select class="form-select" id="category_id" name="category_id" {{ $required }}>
+                                            <select class="form-select" id="category_id" name="category_id_{{ $language->lang_code }}" {{ $required }}>
                                                 <option value="">Kategori Seçiniz</option>
                                                 @foreach($categories as $category)
                                                     <option value="{{ $category->category_id }}">{{ $category->title }}</option>
--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -107,7 +107,7 @@
                                         </div>
                                         <div class="mb-3">
                                             <label for="category_id" class="form-label">Kategori ({{ $language->lang_code }})</label>
-                                            <select class="form-select" id="category_id" name="category_id" required >
+                                            <select class="form-select" id="category_id" name="category_id_{{ $language->lang_code }}" required >
                                                 <option value="">Kategori Seçiniz</option>
                                                 @foreach($categories as $category)
                                                     <option value="{{ $category->category_id }}" {{ $category->category_id == $product->category_id ? 'selected' : '' }}>{{ $category->title }}</option>
