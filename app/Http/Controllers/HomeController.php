665b16f365b7ddce69a9aab0af7e7765c6cb26cf
yemliha81 <yemlihaddln@hotmail.com>
2026-05-01 13:59:21 +0300

    Product category test 3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -186,7 +186,7 @@ class HomeController extends Controller
 
         if($menu->page_type == 'product_category') {
             if($slug2 == null) {
-                $categories = ProductCategory::where('lang', app()->getLocale())->with('product')->get();
+                $categories = ProductCategory::where('lang', app()->getLocale())->where('parent_category_id', '!=', 0)->orderBy('sort', 'asc')->with('product')->get();
                 $seo = SeoSettings::where('page', 'products')->where('lang', app()->getLocale())->first();
                 dd($categories);
                 return view('product_category', compact('categories', 'menu', 'seo'));
