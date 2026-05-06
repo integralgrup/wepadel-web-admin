4bc98cd3e0951179b4c8a3cff9efebef85944a2e
yemliha81 <yemlihaddln@hotmail.com>
2026-05-06 11:12:19 +0300

    Home about url fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -167,7 +167,7 @@
                         <p>{!!$about->description!!}</p>
                     </div>
                     <div class="button-field flex flex-wrap gap-[25px] mt-[50px]">
-                        <a href="" class="button group min-w-[180px] lg:min-w-[150px] xs:lg:min-w-[120px] justify-center items-center w-fit h-[50px] flex px-[30px] bg-[#0055A3] relative space-x-[10px] transition-all !duration-450 overflow-hidden isolate rounded-full border border-solid border-[#0055A3] before:content before:absolute before:left-[-100%] before:top-0 before:w-full before:h-full before:bg-white hover:before:left-0 before:duration-450 sm:h-[44px] menu-link xs:justify-center ">
+                        <a href="{{$about->button_url}}" class="button group min-w-[180px] lg:min-w-[150px] xs:lg:min-w-[120px] justify-center items-center w-fit h-[50px] flex px-[30px] bg-[#0055A3] relative space-x-[10px] transition-all !duration-450 overflow-hidden isolate rounded-full border border-solid border-[#0055A3] before:content before:absolute before:left-[-100%] before:top-0 before:w-full before:h-full before:bg-white hover:before:left-0 before:duration-450 sm:h-[44px] menu-link xs:justify-center ">
                             <div class="text-[18px]  xs:text-[16px] font-normal font-inter flex items-center text-white group-hover:text-[#0055A3] relative z-2 duration-450 w-max">{{$about->button_text}}</div>
                         </a>
                     </div>
